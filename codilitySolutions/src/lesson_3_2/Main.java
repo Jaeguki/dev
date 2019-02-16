125aa6c37ce2f7d3bcc82fb929bc47410d7632fa
Jaeguki <dev.jaeguki@gmail.com>
2019-02-16 15:57:17 +0900

    lesson_3_2 시간복잡도가 오래걸려서 HashMap을 이용하여 containskey를 통해 충돌이 안 나던 키를 찾던 알고리즘에서, 정렬된 배열에서 발견되지 않는 인덱스를 찾는 형식으로 변경

--- codilitySolutions/src/lesson_3_2/Main.java
+++ codilitySolutions/src/lesson_3_2/Main.java
@@ -3,7 +3,7 @@
  * codility lesson3_1 문제
  * 해결을 위한 Main 클래스
  *
- * @version     1.00 02/16/19
+ * @version     1.01 02/16/19
  * @author      유재국
  * @since       JDK1.8
  */
@@ -13,6 +13,9 @@ public class Main {
 	public static void main(String args[]) {
 		Solution s = new Solution();
 		int[] A = {2,3,1,5};
-		System.out.println(s.solution(A));
+		if ((s.solution(A)) == 4)
+			System.out.println("correct");
+		else
+			System.out.println("wrong");
 	}
 }
--- codilitySolutions/src/lesson_3_2/Solution.java
+++ codilitySolutions/src/lesson_3_2/Solution.java
@@ -3,30 +3,25 @@
  * codility lesson3_2 문제
  * 해결을 위한 Solution 클래스
  *
- * @version     1.00 02/16/19
+ * @version     1.01 02/16/19
  * @author      유재국
  * @since       JDK1.8
  */
 package lesson_3_2;
 
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class Solution {
 	public int solution(int[] A) {
-		HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
-		
-		for(int i = 0; i < A.length; i++) {
-			hm.put(i, A[i]);
+        Arrays.sort(A);
+		for(int i =0; i < A.length+1; i++) {
+			try {
+				if(i+1 != A[i])
+					return i+1;
+			}catch(Exception e) {
+				return i+1;
+			}
 		}
-
-		for(int i = 1; i < A.length+2; i++) {
-			if(hm.containsValue(i)) {
-				continue;
-			}else
-				return i;
-			
-		}
-		
 		return 0;
 	}
 }
