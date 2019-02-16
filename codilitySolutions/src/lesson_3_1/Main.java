9f2bd72ec0d510df51f97b0817cc7b79603fea40
Jaeguki <dev.jaeguki@gmail.com>
2019-02-16 15:52:54 +0900

    lesson_3_1 주석 추가

--- codilitySolutions/src/lesson_3_1/Main.java
+++ codilitySolutions/src/lesson_3_1/Main.java
@@ -1,3 +1,12 @@
+/**
+ * 이 클래스는
+ * codility lesson3_1 문제
+ * 해결을 위한 Main 클래스
+ *
+ * @version     1.00 02/16/19
+ * @author      유재국
+ * @since       JDK1.8
+ */
 package lesson_3_1;
 
 public class Main {
