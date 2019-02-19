72d36e1094546e6f4d38ab1958e3f7baf0bf7970
Jaeguki <dev.jaeguki@gmail.com>
2019-02-19 15:07:49 +0900

    lesson_4_3 테스트케이스 수정

--- codilitySolutions/src/lesson_4_3/Main.java
+++ codilitySolutions/src/lesson_4_3/Main.java
@@ -1,9 +1,22 @@
+/**
+ * 이 클래스는
+ * codility lesson4_3 문제
+ * 해결을 위한 Main 클래스
+ *
+ * @version     1.01 02/19/19
+ * @author      유재국
+ * @since       JDK1.8
+ */
+
 package lesson_4_3;
 
 public class Main {
 	public static void main(String args[]) {
 		Solution s = new Solution();
 		int[] A = {1, 3, 6, 4, 1, 2};
-		System.out.println(s.solution(A));
+		if(s.solution(A) == 5)
+			System.out.println("correct");
+		else
+			System.out.println("wrong");
 	}
 }
